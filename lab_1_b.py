61d294b0ad7653f645b367d181bee5e37b6b8164
Иван Мслеников <bdfy.maslenikov@yandex.ru>
2024-02-14 19:58:47 +0300

    Рабочая версия

--- lab_1_b.py
+++ lab_1_b.py
@@ -1,119 +1,56 @@
-from cmath import cos, sin
-from sympy import sin, cos, Matrix, diff, solve, N
+from cmath import cos, sin, sqrt
+from sympy import sin, cos, Matrix, diff, solve, N, symbols
 import numpy as np
-from sympy.abc import x, y
+from sympy.abc import x, y, z
 import matplotlib.pyplot as plt
 
-eps = 1e-8
-
+eps = 1e-3
+ch_eps = 1e-3
 
 def func():
-    return cos(x - 1) * (y + 1) ** 2
+    return sin(10*z)*(z ** 4 + z ** 2 + 1)
 
+def f(num):
+    return func().limit(z, num)
 
-def f(num, c):
-    return func().limit(y, c).limit(x, num)
+def find_next_z(z_n):
+    return z_n - f(z_n) / diff(sin(10*z)*(z ** 4 + z ** 2 + 1), z).limit(z, z_n)
 
-
-def dfunc_dc():
-    return diff(func, y)
-
-
-def dfunc_dx():
-    return diff(func, x)
-
-
-def df_x(num, c):
-    return diff(cos(x - 1) * (y + 1) ** 2, x).limit(y, c).limit(x, num)
-
-
-def df_c(num, c):
-    return diff(cos(x - 1) * (y + 1) ** 2, y).limit(y, c).limit(x, num)
-
-
-def recurent_find(a, b, c):
-    if abs(b - a) < eps:
-        return (a + b) / 2
-    if f(a, c) * f((a + b) / 2, c) <= 0:
-        return recurent_find(a, (a + b) / 2, c)
-    else:
-        if f((a + b) / 2, c) * f(b, c) <= 0:
-            return recurent_find((a + b) / 2, b, c)
-        else:
-            return None
-
-
-def find_list_ans(N, a, b, c):
-    ans = []
-    for i in range(N):
-        cur = 0
-        # print(i)
-        if f(a + (b - a) / N * i, c) * f(a + (b - a) / N * (i + 1), c) <= 0:
-            cur = recurent_find(a + (b - a) / N * i, a + (b - a) / N * (i + 1), c)
-        if cur is not None and cur != 0:
-            ans.append(cur)
-    return ans
-
-
-def check(cur, next):
-    # print('zdec')
-    if len(cur) != len(next):
-        return 0
-    for i in range(len(cur)):
-        if abs(cur[i] - next[i]) > eps * 3:
+def check(roots, z_0):
+    for i in roots:
+        if dist(i, z_0) < ch_eps:
             return 0
     return 1
 
+def dist(a, b):
+    a = complex(a)
+    b = complex(b)
+    return sqrt((a.real - b.real) * (a.real - b.real) + (a.imag - b.imag) * (a.imag - b.imag)).real
 
-def find_aswers(a, b, c):
-    N = 100
-    cur = find_list_ans(N, a, b, c)
-    next = find_list_ans(2 * N, a, b, c)
-    while not check(cur, next) and N < 1e8:
-        cur = next
-        N *= 2
-        next = find_list_ans(N, a, b, c)
-        print(N)
-    # print(len(cur))
-    # for i in cur:
-    #     print(i, end=' ')
-    if N >= 1e8:
-        return None
-    else:
-        return cur
+def find_roots(a, dc):
+    x = -a
+    roots = []
+    while x < a:
+        y = -a
+        while y < a:
+            z_0 = complex(x, y)
+            next = complex(find_next_z(z_0))
+            while dist(next, z_0) > eps:
+                z_0 = complex(next)
+                next = complex(find_next_z(z_0))
+            if check(roots, z_0):
+                roots.append(z_0)
+            y += dc
+        x += dc
+    # print(x)
+    return roots
 
-
-def find_roots(a, b, alpha, beta, dc):
-    res = find_aswers(a, b, alpha)
-    # print(res)
-    matrix = []
-    for i in range(len(res)):
-        buf = [res[i]]
-        matrix.append(buf)
-
-    for i in range(len(matrix)):
-        c_0 = alpha
-        x_0 = matrix[i][0]
-        # print(df_x(x_0, c_0))
-        while c_0 < beta:
-            if abs(df_x(x_0, c_0)) < eps * 100:
-                break
-            x_0 = x_0 - df_c(x_0, c_0) / df_x(x_0, c_0)
-            matrix[i].append(x_0)
-            c_0 += dc
-    return matrix
-
-
-def print_roots(matrix, a, dc):
-    for i in range(len(matrix)):
-        buf = []
-        for j in range(len(matrix[i])):
-            buf.append(a + dc * j)
-        plt.plot(buf, matrix[i])
+def print_all(roots,a):
+    for i in roots:
+        if abs(i.real)<=a and abs(i.imag)<=a:
+            plt.plot(i.real,i.imag, 'bo')
     plt.show()
 
-
-a, b, alpha, beta, dc = map(float, input("Введите отрезок a,b,alpha,beta, dc\n").split())
-print_roots(find_roots(a, b, alpha, beta, dc), a, dc)
-# find_roots(a, b, alpha, beta, dc)
-# print(diff(cos(x - 1) * y ** 2, y).limit(y,2).limit(x,5))
+a, dc = map(float, input("Введите отрезок a, dc\n").split())
+print(find_roots(a, dc))
+print_all(find_roots(a,dc),a)
